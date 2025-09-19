e658d79659a341e77e85060c2439b2a7c4c14d4d
Anghelo Carvajal <angheloalf95@gmail.com>
2025-09-19 14:22:20 -0300

    Setup snapshot testing

---
+++ src/gnuv2_demangle/tests/snapshots.rs
@@ -0,0 +1,24 @@
+/* SPDX-FileCopyrightText: © 2025 Decompollaborate */
+/* SPDX-License-Identifier: MIT OR Apache-2.0 */
+
+use gnuv2_demangle::{demangle, DemangleConfig, DemangleError};
+
+fn demangle_lines<'s>(contents: &'s str, config: &DemangleConfig) -> Vec<Result<String, DemangleError<'s>>> {
+    contents.lines().map(|line| demangle(&line, &config)).collect()
+}
+
+#[test]
+fn test_mangled_list_hit_and_run_cfilt() {
+    let contents = include_str!("mangled_lists/hit_and_run.txt");
+    let config = DemangleConfig::new();
+
+    insta::assert_debug_snapshot!(demangle_lines(contents, &config));
+}
+
+#[test]
+fn test_mangled_list_hit_and_run_improved() {
+    let contents = include_str!("mangled_lists/hit_and_run.txt");
+    let config = DemangleConfig::new_no_cfilt_mimics();
+
+    insta::assert_debug_snapshot!(demangle_lines(contents, &config));
+}
