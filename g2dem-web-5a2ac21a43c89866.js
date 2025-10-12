b954d5215f2ff86afc6a033725897aa61c06f9e2
AngheloAlf <AngheloAlf@users.noreply.github.com>
2025-10-12 20:57:06 +0000

    deploy: 451aa2930b73add72271c624eb07ac2d965d8236

--- g2dem-web-5a2ac21a43c89866.js
+++ g2dem-web-5a2ac21a43c89866.js
@@ -252,7 +252,7 @@ function __wbg_adapter_6(arg0, arg1, arg2) {
     wasm.closure135_externref_shim(arg0, arg1, arg2);
 }
 
-function __wbg_adapter_11(arg0, arg1, arg2) {
+function __wbg_adapter_9(arg0, arg1, arg2) {
     wasm.closure185_externref_shim(arg0, arg1, arg2);
 }
 
@@ -686,7 +686,7 @@ function __wbg_get_imports() {
     };
     imports.wbg.__wbindgen_cast_b4887078b2fede2c = function(arg0, arg1) {
         // Cast intrinsic for `Closure(Closure { dtor_idx: 184, function: Function { arguments: [Externref], shim_idx: 185, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-        const ret = makeMutClosure(arg0, arg1, 184, __wbg_adapter_11);
+        const ret = makeMutClosure(arg0, arg1, 184, __wbg_adapter_9);
         return ret;
     };
     imports.wbg.__wbindgen_cast_f21342231ca0a54c = function(arg0, arg1) {
